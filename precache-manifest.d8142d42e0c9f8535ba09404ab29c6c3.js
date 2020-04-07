6500f6881c88a224f37e26b4f2bfb215eb8f4d68
christopher@aitmed.com <pfftdammitchris@gmail.com>
2020-04-07 16:26:36 -0700

    Updates

---
+++ precache-manifest.d8142d42e0c9f8535ba09404ab29c6c3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9a8afd1faccba16e258483b4f113e54",
+    "url": "./index.html"
+  },
+  {
+    "revision": "6ea7c0dd61d6bc2e8bd4",
+    "url": "./static/js/2.f277b471.chunk.js"
+  },
+  {
+    "revision": "077d23cd4672177a120ec7d3a25d0001",
+    "url": "./static/js/2.f277b471.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be0249c28a96f32c6a2a",
+    "url": "./static/js/main.78cd9d98.chunk.js"
+  },
+  {
+    "revision": "dc0181db8383584898dc",
+    "url": "./static/js/runtime-main.6b2bd169.js"
+  }
+]);
